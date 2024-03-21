c6ab59c1324b7b0d67a125c4b2f1955b521b66dc
Lars Schöbitz <lschoebitz@ethz.ch>
2024-03-21 22:22:46 +0100

    update agenda

--- pages/events/2023-01-24-webinar-data-publishing/data/get_course_data.R
+++ pages/events/2023-01-24-webinar-data-publishing/data/get_course_data.R
@@ -10,11 +10,13 @@ library(readr)
 library(dplyr)
 library(lubridate)
 library(stringr)
+library(tidyr)
 
 # script ------------------------------------------------------------------
 
+## PLOS Webinar
 
-sheet <- "https://docs.google.com/spreadsheets/d/1tTCgtxKsFZwvrmYysr34fnDyVtzuWS9BXjuwbENg3bM/edit#gid=0"
+sheet <- "https://docs.google.com/spreadsheets/d/1xEmiCTq0n98fB-m8ArHjTgh55CByTgQH7rKFECY1fRc/edit#gid=0"
 
 googlesheets4::read_sheet(ss = sheet) |>
     select(-duration) |> 
@@ -25,4 +27,37 @@ googlesheets4::read_sheet(ss = sheet) |>
     mutate(end_time = as.character(end_time)) |>
     mutate(end_time = str_extract(end_time, "\\b\\d{2}:\\d{2}\\b")) |>
     mutate(time = paste(start_time, end_time, sep = " - ")) |> 
-    write_csv(here::here("pages/events/2023-01-24-webinar-data-publishing/data/tbl-05-washweb-webinar-agenda.csv"))
+    write_csv(here::here("pages/events/2023-01-24-webinar-data-publishing/data/tbl-07-webinar-data-sharing-agenda.csv"))
+
+## WASHWeb webinar
+
+sheet_washweb <- "https://docs.google.com/spreadsheets/d/1tTCgtxKsFZwvrmYysr34fnDyVtzuWS9BXjuwbENg3bM/edit#gid=0"
+
+tbl_washweb <- googlesheets4::read_sheet(ss = sheet_washweb) |>
+    select(-duration) |> 
+    mutate(speaker_1 = case_when(
+        is.na(speaker_1) == FALSE ~ paste0("[", speaker_1, "](", speaker_link_1, "/)"),
+        TRUE ~ " "
+    )) |> 
+    mutate(speaker_2 = case_when(
+        is.na(speaker_2) == FALSE ~ paste0("/ [", speaker_2, "](", speaker_link_2, "/)"),
+        TRUE ~ " "
+    )) |>
+    mutate(affiliation = paste0("[", affiliation, "](", affiliation_link, "/)")) |>
+    mutate(affiliation_2 = case_when(
+        is.na(affiliation_2) == FALSE ~ paste0("/ [", affiliation_2, "](", speaker_link_2, "/)"),
+        TRUE ~ " "
+    )) |>
+    mutate(start_time = as.character(start_time)) |>
+    mutate(start_time = str_extract(start_time, "\\b\\d{2}:\\d{2}\\b")) |>
+    mutate(end_time = as.character(end_time)) |>
+    mutate(end_time = str_extract(end_time, "\\b\\d{2}:\\d{2}\\b")) |>
+    mutate(time = paste(start_time, end_time, sep = " - ")) |> 
+    unite(col = "speaker", c(speaker_1, speaker_2), sep = " ") |> 
+    unite(col = "affiliation", c(affiliation, affiliation_2), sep = " ")
+
+
+tbl_washweb |> 
+    write_csv(here::here("pages/events/2024-04-04-webinar-washweb/data/tbl-05-washweb-webinar-agenda.csv"))
+
+
