7bcc75b137b225345f35e1b2289173b33e76fd98
Lars Schöbitz <lschoebitz@ethz.ch>
2024-07-04 13:49:16 +0200

    swap events listing to gsheet df

---
+++ pages/events/events_data/write_yaml_file.R
@@ -0,0 +1,20 @@
+
+library(yaml)
+library(googlesheets4)
+library(tidyverse)
+
+# source functions --------------------------------------------------------
+
+source(here::here("R/df_to_yaml.R"))
+
+# read data ---------------------------------------------------------------
+
+read_sheet("1CZJuDshsUv6A-N-2fwzMElndG8Oig3N8PSv4h5lmefU") |> 
+    write_csv(here::here("pages/events/events_data/tbl-11-openwashdata-events.csv"))
+
+events <- read_csv(here::here("pages/events/events_data/tbl-11-openwashdata-events.csv")) |> 
+    mutate(date = lubridate::as_date(date))
+
+# functions
+
+df_to_yaml(events, here::here("pages/events/events_data/events.yml"))
